40f41748d63595d6ea91984b666081dcdc9c21ea
devanfer02 <ubdevanferrel04@student.ub.ac.id>
2024-03-26 17:56:43 +0700

    fix: source code pertemuan 4

--- 4-Threads/threadsatu.c
+++ 4-Threads/threadsatu.c
@@ -1,39 +1,31 @@
 #include <stdio.h>
-#include <string.h>
 #include <pthread.h>
 #include <stdlib.h>
 
-pthread_t tid[2];
-
-void* doSomeThing(void *arg) {
-    unsigned long i = 0;
-
-    pthread_t id = pthread_self();
-    if(pthread_equal(id, tid[0])) 
+void* thread1()
+{
+    while(1)
     {
-        printf("\n First thread processing\n");
-    } else {
-        printf("\n Secind thread processing\n");
+        printf("Hello\n");
     }
-
-    for(i = 0; i<(0xFFFFFFFF); i++);
-    return NULL;
 }
 
-int main(void) 
+void* thread2() 
 {
-    int i =0;
-    int err;
-    while(i < 2) 
+    while(1)
     {
-        err = pthread_create(&(tid[i]), NULL, &doSomeThing, NULL);
-        if (err != 0) 
-            printf("\n cant create thread :[%s]", strerror(err));
-        else 
-            printf("\n Thread create successfully")
-        i++;
+        printf("How are you\n");
     }
+}
 
-    sleep(5);
+int main() 
+{
+    int status;
+    pthread_t tid1, tid2;
+
+    pthread_create(&tid1, NULL, thread1, NULL);
+    pthread_create(&tid2, NULL, thread2, NULL);
+    pthread_join(tid1,NULL);
+    pthread_join(tid2,NULL);
     return 0;
 }
